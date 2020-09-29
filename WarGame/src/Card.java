c0cfc7454f57eaaa6d4e663424af323c8aa20fd8
Elgin5 <60816195+Elgin5@users.noreply.github.com>
2020-09-29 23:58:54 +0800

    First push, added WarGame java file.

---
+++ WarGame/src/Card.java
@@ -0,0 +1,318 @@
+import java.util.AbstractList;
+import java.util.Scanner;
+
+public class Card<T> extends AbstractList<T>{
+	
+	private Node head;
+	
+	private class Node {
+		
+		private Node next;
+		private T info;
+		
+		public Node(T info) {
+			
+			this.info = info;
+			
+		}
+		
+		public Node (T info, Node next) {
+			
+			this.next = next;
+			this.info = info;
+			
+		}
+		
+	}
+	
+	public boolean add(T info) {
+		
+		if (head == null) {
+			
+			head = new Node(info);
+			return true;
+			
+		}
+		
+		Node curr = head;
+		
+		while (curr.next != null) {
+			
+			curr = curr.next;
+			
+		}
+		
+		curr.next = new Node(info);
+		
+		return true;
+		
+	}
+	
+	public boolean addNode(int i, T info) {
+		
+		if (head == null) {
+			
+			head = new Node(info);
+			return true;
+			
+		}
+		
+		Node curr = head;
+		Node prev = null;
+		
+		int index = 0;
+		
+		while (index < i && curr.next != null) {
+			
+			prev = curr;
+			curr = curr.next;
+			
+			index++;
+			
+		}
+		
+		Node temp = new Node(info);
+		
+		if (i == 0) {
+			
+			temp.next = head;
+			head = temp;
+			
+			return true;
+			
+		}
+		
+		temp.next = curr;
+		prev.next = temp;
+		
+		return true;
+		
+	}
+	
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		Node curr = head;
+		
+		for (int i = 0; i < index; i++) {
+			
+			curr = curr.next;
+			
+		}
+		
+		return curr.info;
+		
+	}
+	
+	// index must be within our list
+	public T remove (int index) {
+		
+		Node curr = head;
+		
+		if (index == 0) {
+			
+			curr = curr.next;
+			Node temp = curr.next;
+			curr.next = curr;
+			return temp.info;
+			
+		} else {
+		
+			for (int i = 0; i < index-1; i++) {
+			
+				curr = curr.next;
+			
+			}
+		
+			Node temp = curr.next;
+			curr.next = curr.next.next;
+		
+			return temp.info;
+			
+		}
+		
+	}
+
+	@Override
+	public int size() {
+
+		int size = 0;
+		
+		Node curr = head;
+		
+		while(curr.next != null) {
+			
+			curr = curr.next;
+			size++;
+			
+		}
+		
+		return size;
+	}
+	
+	public Card<Integer> shuffle (Card<Integer> initialDeck) {
+		
+		for (int i = 1; i <= 52; i++) {
+			
+			int random1 = (int)(Math.random()*52);
+			int random2 = (int)(Math.random()*52);
+			
+			int temp = initialDeck.get(random1);
+			int temp1 = initialDeck.get(random2);
+			int temp3 = 0;
+			
+			temp3 = temp;
+			temp = temp1;
+			temp1 = temp3;
+			
+			initialDeck.remove(random1);
+			initialDeck.addNode(random1, temp);
+			
+			initialDeck.remove(random2);
+			initialDeck.addNode(random2, temp1);
+			
+		}
+		
+		
+		return initialDeck;
+		
+	}
+	
+	public void playGame (Card<Integer> cardDeck) {
+		
+		Card<Integer> player1 = new Card<Integer>();
+		Card<Integer> player2 = new Card<Integer>();
+		
+		boolean winGame = false;
+		
+		boolean start = true;
+		
+		
+			for (int i = 0; i < 26; i++) {
+			
+				player1.add(cardDeck.get(i));
+			
+			}
+		
+			for (int i = 26; i < 52; i++) {
+			
+				player2.add(cardDeck.get(i));
+			
+			}
+			
+		
+		
+		//System.out.println(player1);
+		//System.out.println(player2);
+		
+		Scanner in = new Scanner(System.in);
+		
+		while (!winGame) {
+				
+			System.out.println("Player 1 has "+(player1.size()+1)+" cards, and Player 2 has "+(player2.size()+1)+" cards.");
+			System.out.println("Press enter to flip cards");
+			String enter = in.nextLine();
+			
+			if (enter.equals("")) {
+					
+				System.out.println("Player 1 played a "+player1.get(0));
+				System.out.println("Player 2 played a "+player2.get(0));
+					
+				if (player1.get(0) > player2.get(0)) {
+						
+					player1.add(player1.get(0));
+					player1.add(player2.get(0));
+					player1.remove(0);
+					player2.remove(0);
+					
+					System.out.println(player1.get(0));
+					System.out.println("Player 1 Wins the Round!");
+						
+				} else if (player2.get(0) > player1.get(0)) {
+						
+					player2.add(player2.get(0));
+					player2.add(player1.get(0));
+					player2.remove(0);
+					player1.remove(0);
+					
+					System.out.println(player2.get(0));
+					System.out.println("Player 2 Wins the Round!");
+						
+				}
+					
+			} 
+		}
+			
+		if (player1.size() == 0) {
+				
+			System.out.println("Player 1 Wins!");				
+			winGame = true;
+				
+			
+		} else if (player2.size() == 0) {
+				
+			System.out.println("Player 2 Wins");
+			winGame = true;
+				
+		}
+				
+	}
+		
+	
+	
+	public String toString() {
+		
+		if (size() == 0) return "[ ]";
+		String output = "[";
+		Node curr = head;
+		while (curr != null) {
+			output += curr.info.toString() + ", ";
+			curr = curr.next;
+		}
+		return output.substring(0,output.length()-2) +"]";
+		
+	}
+	
+	
+	public static void main (String[] args) {
+		
+		Card<Integer> initialDeck = new Card<Integer>();
+		Card<Integer> testDeck = new Card<Integer>();
+		
+		for (int i = 1; i <= 13; i++) {
+			
+			for (int j = 1; j <= 4; j++) {
+				
+				initialDeck.add(i);
+				
+			}
+			
+		}
+		
+		//initialDeck.shuffle(initialDeck);
+		//initialDeck.shuffle(initialDeck);
+		//initialDeck.shuffle(initialDeck);
+		
+		//System.out.println(initialDeck);
+		//initialDeck.playGame(initialDeck);
+		//System.out.println(initialDeck.get(0));
+		//initialDeck.remove(0);
+		//System.out.println(initialDeck);
+		
+		testDeck.add(1);
+		testDeck.add(2);
+		testDeck.add(3);
+		testDeck.add(4);
+		testDeck.add(5);
+		testDeck.add(6);
+		testDeck.add(7);
+		System.out.println(testDeck);
+		testDeck.remove(0);
+		System.out.println(testDeck);
+		
+		
+	}
+
+
+	
+}
